04587ade2a521377a252dfeb6f0100fb8c08b3cb
Sean <sean@albinodrought.com>
2022-04-02 12:40:02 -0700

    chore: bundle includes nicer docs

--- bundle.sh
+++ bundle.sh
@@ -8,5 +8,7 @@ OUTPUT=creamy-miner_$(date +"%Y-%m-%d").zip
 rm -f creamy-miner
 CGO_ENABLED=0 $(go env GOPATH)/bin/garble -literals build -ldflags "-s -w" -o creamy-miner
 
+pandoc -V geometry:margin=1in -f markdown -t html -o README.html README.md
+
 rm -f $OUTPUT
-7z a $OUTPUT creamy-miner README.md LICENSE
+7z a $OUTPUT creamy-miner README.md README.html LICENSE
